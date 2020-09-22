851c29eea26f088f26d4830b26fe73d04ca310b9
黄炜杰 <wjhuang@live.cn>
2020-09-22 22:43:37 +0800

    标题

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,16 @@ export default function Home({ posts }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>黄炜杰的博客 | Gerald Huang's blog</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href='http://cdn.repository.webfont.com/webfonts/nomal/140999/29782/5f6a0639f629d80cc02be8c2.css' rel='stylesheet' type='text/css' />
+        <link href='http://cdn.repository.webfont.com/webfonts/nomal/140999/47082/5f6a0561f629d80cc02be8be.css' rel='stylesheet' type='text/css' />
       </Head>
-
+      <header>
+        <h1 className={styles.title}>黄炜杰的博客</h1>
+        <div className={styles.subtitle}>GERALD HUANG</div>
+      </header>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          站点玩命建设中。。。
-        </h1>
-        <br />
         {posts.map(post => <Link key={`post-${post.path}`} href="/posts/[path]" as={`/posts/${post.path}`}><a>{post.title}</a></Link>)}
       </main>
     </div>
