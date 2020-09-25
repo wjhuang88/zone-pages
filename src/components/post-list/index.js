6d260d6495689afe07324f5330ddde559a113ebb
黄炜杰 <wjhuang@live.cn>
2020-09-25 23:31:40 +0800

    列表页样式

--- src/components/post-list/index.js
+++ src/components/post-list/index.js
@@ -1,12 +1,12 @@
+import moment from 'dayjs'
 import PostListItem from './item'
-
 import styles from './PostList.module.css'
 
 export default function PostList({ posts }) {
   return (
     <ul className={styles.postList}>
       {posts.map(post => (
-        <PostListItem key={post.path} path={post.path} title={post.title} des={post.des} />
+        <PostListItem key={post.path} path={post.path} title={post.title} des={post.des} date={moment()} />
       ))}
     </ul>
   );
--- src/components/post-list/item.js
+++ src/components/post-list/item.js
@@ -1,14 +1,28 @@
 import Link from 'next/link'
 
+import { FireIcon } from './icons'
 import styles from './PostList.module.css'
 
-export default function PostListItem({ path, title, des }) {
+export default function PostListItem({ path, title, date, des, img }) {
+  const image = img || 'https://images.pexels.com/photos/1684149/pexels-photo-1684149.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=150'
+
   return (
     <li className={styles.postItem}>
       <Link key={`post-${path}`} href="/posts/[path]" as={`/posts/${path}`}>
-        <a className={styles.title}>{title}</a>
+        <a className={styles.postImage}>
+          <div style={{ backgroundImage: `url(${image})` }} />
+        </a>
       </Link>
-      <div className={styles.des}>{des}</div>
+      <div className={styles.postBlock}>
+        <Link key={`post-${path}`} href="/posts/[path]" as={`/posts/${path}`}>
+          <a className={styles.title}>{title}</a>
+        </Link>
+        <p className={styles.des}>{des}</p>
+        <div className={styles.footer}>
+          于 {date.format('YYYY-MM-DD')}
+          <FireIcon style={{ marginLeft: 15 }} value="99,999" />
+        </div>
+      </div>
     </li>
   )
 }
