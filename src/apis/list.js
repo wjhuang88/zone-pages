11fb6f9b5c1f644a4e36abfdf1f38af1e33e7635
黄炜杰 <wjhuang@live.cn>
2023-10-30 16:48:33 +0800

    去除无效缓存

--- src/apis/list.js
+++ src/apis/list.js
@@ -2,11 +2,11 @@ import { CONTENT_BASE } from '@config'
 import moment from 'dayjs'
 
 export async function getPostList() {
-  const catRes = await fetch(`${CONTENT_BASE}/notebooks.json`)
+  const catRes = await fetch(`${CONTENT_BASE}/notebooks.json`, { cache: 'no-cache' })
   const catJson = await catRes.json()
   const result = []
   for (let cat of catJson) {
-    const catListRes = await fetch(`${CONTENT_BASE}/${cat.dir.split('/').filter(word => word.match(/\w+/)).join('/')}/meta.json`)
+    const catListRes = await fetch(`${CONTENT_BASE}/${cat.dir.split('/').filter(word => word.match(/\w+/)).join('/')}/meta.json`, { cache: 'no-cache' })
     const catListJson = await catListRes.json()
     for (let post of catListJson) {
       result.push({
