e0bee0a226c6ed042aa72876b778a34f2f85fdc0
黄炜杰 <wjhuang@live.cn>
2024-03-01 10:41:08 +0800

    测试

--- src/app/posts/[path]/page.js
+++ src/app/posts/[path]/page.js
@@ -2,8 +2,8 @@ import { getPost, getMeta } from '@/apis'
 import Post from './post'
 
 export default async function Page({ params }) {
-  const postHtml = await getPost(params.path, params.update_time)
   const postMeta = await getMeta(params.path)
+  const postHtml = await getPost(params.path, postMeta.update_time)
   const postData = {
     ...postMeta,
     contentHtml: postHtml.replace(/<img\s+src\s*="(.*)"/g, `<img src="/assets?img=${postMeta.parent}/$1"`)
