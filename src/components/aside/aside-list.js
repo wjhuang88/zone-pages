7fb7b243e68b8a0ae8b0f756d00b043a13dde485
黄炜杰 <wjhuang@live.cn>
2024-05-06 15:03:35 +0800

    修正动画过程的图片宽度问题

--- src/components/aside/aside-list.js
+++ src/components/aside/aside-list.js
@@ -6,7 +6,7 @@ export default function AsideList({ posts }) {
     <ul className={styles.list}>
       {posts?.map((item, index) => (
         <li style={{marginTop: index !== 0 ? 8 : 0}} key={index}>
-          <img src="/assets/icons/article.svg" />
+          <img width="14px" height="14px" src="/assets/icons/article.svg" />
           <Link href="/posts/[path]" as={`/posts/${item.id}`} className={styles.link}>
             {item.title}
           </Link>
