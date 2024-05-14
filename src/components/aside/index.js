356e1c598c25510bec9b6f86973c942d0ade7b6f
黄炜杰 <wjhuang@live.cn>
2024-05-14 15:18:42 +0800

    文章字体调整

--- src/components/aside/index.js
+++ src/components/aside/index.js
@@ -14,13 +14,15 @@ export default function Aside({ recommendPosts, latestPosts }) {
         <h2>铭</h2>
         <p>人活着是没有意义的，但是，活下去的话说不定会找到有趣的事。</p>
       </div>
-      <div className={styles.asidePanel + ' ' + styles.anim} style={{ animationDuration: (baseDuration + 0.1) + 's' }}>
-        <h2>推荐</h2>
-        <AsideList posts={recommendPosts} />
-      </div>
-      <div className={styles.asidePanel + ' ' + styles.anim} style={{ animationDuration: (baseDuration + 0.2) + 's' }}>
-        <h2>最新发表</h2>
-        <AsideList posts={latestPosts} />
+      <div>
+        <div className={styles.asidePanel + ' ' + styles.anim} style={{ animationDuration: (baseDuration + 0.1) + 's' }}>
+          <h2>推荐</h2>
+          <AsideList posts={recommendPosts} />
+        </div>
+        <div className={styles.asidePanel + ' ' + styles.anim} style={{ animationDuration: (baseDuration + 0.2) + 's' }}>
+          <h2>最新发表</h2>
+          <AsideList posts={latestPosts} />
+        </div>
       </div>
     </div>
   )
