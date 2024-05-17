36cb5b60a6456e5adbbc5650d3a1620e2bcafc90
黄炜杰 <wjhuang@live.cn>
2024-05-17 14:36:04 +0800

    文章图片改为后台代理

--- src/app/assets/route.js
+++ src/app/assets/route.js
@@ -1,11 +1,15 @@
-import { redirect } from 'next/navigation'
 import { CONTENT_BASE } from '@config'
+
+
+async function fetchImage(img) {
+  if (img.startsWith('http://') || img.startsWith('https://')) {
+    return await fetch(img)
+  }
+  return await fetch(`${CONTENT_BASE}/${img}`)
+}
 
 export async function GET(req) {
   const { searchParams } = new URL(req.url)
   const img = searchParams.get('img')
-  if (img.startsWith('http://') || img.startsWith('https://')) {
-    return redirect(img)
-  }
-  return redirect(`${CONTENT_BASE}/${img}`)
+  return await fetchImage(img)
 }
