833b7e5c34428b40940850a2c0dc38c8fedf6298
黄炜杰 <wjhuang@live.cn>
2024-07-05 16:13:04 +0800

    优化动画效果

--- src/widgets/cat-widget/index.js
+++ src/widgets/cat-widget/index.js
@@ -1,5 +1,13 @@
+"use client"
+
+import { useState } from 'react'
 import styles from './CatWidget.module.scss'
 
 export default function CatWidget() {
-  return <div className={styles.container}></div>
+
+  const [pressed, setPressed] = useState(false)
+
+  const finalClass = pressed ? `${styles.container} ${styles.pressed}` : styles.container
+
+  return <div onMouseDown={() => setPressed(true)} onMouseUp={() => setPressed(false)} className={finalClass}></div>
 }
